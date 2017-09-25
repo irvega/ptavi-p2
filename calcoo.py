ba17553afa8cd113c438e8dc515dbb9181b99d6c
Irene Vega Lago <ivega@gamma15.aulas.gsyc.urjc.es>
2017-09-25 14:44:28 +0200

    suma/resta

--- calcoo.py
+++ calcoo.py
@@ -13,19 +13,18 @@ class Calculadora():
   def suma(self):
     return self.valor1+self.valor2
    
-
   def resta(self):
     return self.valor1-self.valor2
     
 if __name__ == "__main__":
-    Op1=Calculadora(int(sys.argv[1]),int(sys.argv[3]))
     if len(sys.argv) !=4:
         sys.exit('Se usa así: python3 calcoo.py operando1 operador operando2')
+
+    Op1=CalculadoraHija(int(sys.argv[1]),int(sys.argv[3]))
 
     if sys.argv[2] == "suma":
         resul = Op1.suma()
     elif sys.argv[2] == "resta":
         resul = Op1.resta()
-    else:
-        sys.exit('Operación sólo suma o resta')
-print (resul)
+
+    print(resul)
