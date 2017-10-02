1a3653e7d2e95b92d8ded86436d5006310efa97d
Irene Vega Lago <ivega@beta14.aulas.gsyc.urjc.es>
2017-10-02 15:17:58 +0200

    import

--- calcplusplus.py
+++ calcplusplus.py
@@ -3,6 +3,7 @@
 
 import sys
 import csv
+from calcoohija import CalculadoraHija
 
 with open(sys.argv[1], 'r') as fich:
     lineas = csv.reader(fich)
@@ -11,24 +12,33 @@ with open(sys.argv[1], 'r') as fich:
         operador = operaciones[0]  # ["suma", 1, 2, 3, 4, 5] -> "suma"
         resul = int(operaciones[1])  # ["suma", 1, 2, 3, 4, 5] -> 1
 
+        lista = []
+        for numero in operaciones[1:]:
+            try:
+                numero = int(numero)
+                lista.append(numero)
+            except ValueError:
+                sys.exit("Error: No numero")
+
         if operador == 'suma':
-            for operando in operaciones[2:]:
-                resul += int(operando)
+            for operando in lista[1:]:
+                op = CalculadoraHija(resul,operando)
+                resul = op.suma()
             print(resul)
 
         elif operador == 'resta':
-            for operando in operaciones[2:]:
-                resul -= int(operando)
+            for operando in lista[1:]:
+                op = CalculadoraHija(resul,operando)
+                resul = op.resta()
             print(resul)
         if operador == 'multiplica':
-            for operando in operaciones[2:]:
-                resul *= int(operando)
+            for operando in lista[1:]:
+                op = CalculadoraHija(resul,operando)
+                resul = op.multiplica()
             print(resul)
 
         elif operador == 'divide':
-            for operando in operaciones[2:]:
-                try:
-                    resul /= int(operando)
-                except ZeroDivisionError:
-                    sys.exit('  Division by zero is not allowed')
+            for operando in lista[1:]:
+                op = CalculadoraHija(resul,operando)
+                resul = op.divide()
             print(resul)
