d4c5852b866aecfd1ddf402f849d478ebcbead0f
Irene Vega Lago <ivega@gamma07.aulas.gsyc.urjc.es>
2017-10-02 16:01:54 +0200

    import

--- calcplus.py
+++ calcplus.py
@@ -3,32 +3,48 @@
 
 import sys
 import csv
+from calcoohija import CalculadoraHija
+
+if len(sys.argv) != 2:
+    sys.exit("  Use: python3 calcplusplus.py <fich>")
 
 with open(sys.argv[1], 'r') as fich:
+
     lineas = csv.reader(fich)
 
     for operaciones in lineas:
+
+        lista = []
+        for numero in operaciones[1:]:
+            try:
+                numero = int(numero)
+                lista.append(numero)
+            except ValueError:
+                sys.exit("Error: Not number")
+
+        if len(lista) == 0:
+            resul = 0
+        else:
+            resul = int(operaciones[1])  # ["suma", 1, 2, 3, 4, 5] -> 1
+
         operador = operaciones[0]  # ["suma", 1, 2, 3, 4, 5] -> "suma"
-        resul = int(operaciones[1])  # ["suma", 1, 2, 3, 4, 5] -> 1
 
         if operador == 'suma':
-            for operando in operaciones[2:]:
-                resul += int(operando)
-            print(resul)
+            for operando in lista[1:]:
+                op = CalculadoraHija(resul, operando)
+                resul = op.suma()
 
         elif operador == 'resta':
-            for operando in operaciones[2:]:
-                resul -= int(operando)
-            print(resul)
+            for operando in lista[1:]:
+                op = CalculadoraHija(resul, operando)
+                resul = op.resta()
         if operador == 'multiplica':
-            for operando in operaciones[2:]:
-                resul *= int(operando)
-            print(resul)
+            for operando in lista[1:]:
+                op = CalculadoraHija(resul, operando)
+                resul = op.multiplica()
 
         elif operador == 'divide':
-            for operando in operaciones[2:]:
-                try:
-                    resul /= int(operando)
-                except ZeroDivisionError:
-                    sys.exit('  Division by zero is not allowed')
-            print(resul)
+            for operando in lista[1:]:
+                op = CalculadoraHija(resul, operando)
+                resul = op.divide()
+        print(resul)
