50fd6766eb2c7b3e5d6eeb503620e47980a059d8
Irene Vega Lago <ivega@gamma17.aulas.gsyc.urjc.es>
2017-10-02 10:19:24 +0200

    .

--- esqueleto-oo.py
+++ esqueleto-oo.py
@@ -4,10 +4,14 @@
 class Clase(ClaseMadre):
   "Esto es un ejemplo de clase que hereda de ClaseMadre"
 
-  def __init__(self, valor):
+  def __init__(self, valor1,valor2):
     "Esto es el método iniciliazador"
-    self.atributo = valor
+    self.valor1 = valor1
+    self.valor2 = valor2
 
+  def suma(self):
+    resul=self.valor1+self.valor2
+    
 if __name__ == "__main__":
   objeto = Clase("pepe") # Creo un objeto de la clase Clase
                          # y le paso el valor pepe para su
