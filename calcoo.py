978584d89c08fc91735c5f0ddbba787777b29a54
Irene Vega Lago <ivega@gamma17.aulas.gsyc.urjc.es>
2017-10-02 10:16:38 +0200

    pep8 suma/resta

--- calcoo.py
+++ calcoo.py
@@ -3,34 +3,35 @@
 
 import sys
 
+
 class Calculadora():
 
-  def __init__(self, valor1,valor2):
-    "Esto es el método iniciliazador"
-    self.valor1 = valor1
-    self.valor2 = valor2
+    def __init__(self, valor1, valor2):
+        "Esto es el método iniciliazador"
+        self.valor1 = valor1
+        self.valor2 = valor2
 
-  def suma(self):
-    return self.valor1+self.valor2
-   
-  def resta(self):
-    return self.valor1-self.valor2
-  
+    def suma(self):
+        return self.valor1+self.valor2
+
+    def resta(self):
+        return self.valor1-self.valor2
+
 if __name__ == "__main__":
-    if len(sys.argv) !=4:
-        sys.exit('  Se usa así: python3 calcoo.py operando1 operador operando2')
+    if len(sys.argv) != 4:
+        sys.exit('  Usar así: python3 calcoo.py operando1 operador operando2')
     try:
-        operando1=int(sys.argv[1])
-        operando2=int(sys.argv[3])
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
     except ValueError:
         sys.exit('  Operando1 y Operando2 deben ser numeros enteros')
 
-    Op1=Calculadora(int(sys.argv[1]),int(sys.argv[3]))
+    Op1 = Calculadora(int(sys.argv[1]), int(sys.argv[3]))
 
     if sys.argv[2] == "suma":
         resul = Op1.suma()
     elif sys.argv[2] == "resta":
         resul = Op1.resta()
     else:
-        sys.exit('  Operación sólo suma o resta')
+        sys.exit('  Operación sólo "suma" o "resta"')
     print(resul)
